d8ee6f81c1390d95f63f12de25f1c90838753418
carolina-hirschheimer <carolinah@al.insper.edu.br>
2021-04-19 16:32:15 -0300

    questao 3 prova

--- src/br/edu/insper/desagil/data/DataFrame.java
+++ src/br/edu/insper/desagil/data/DataFrame.java
@@ -126,21 +126,5 @@ public class DataFrame {
 		m = s / values.size();
 
 		return Math.sqrt(m);
-	}package br.edu.insper.desagil.terminal;
-
-import javax.swing.JFrame;
-
-public class Terminal {
-	public static void main(String[] args) {
-		javax.swing.SwingUtilities.invokeLater(() -> {
-			View view = new View();
-
-			JFrame window = new JFrame("Terminal");
-			window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			window.setContentPane(view);
-			window.setResizable(false);
-			window.setVisible(true);
-			window.pack();
-		});
 	}
 }
